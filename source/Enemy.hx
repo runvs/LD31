9ffacbee73d3dfb0a7f36ee5c93843aad2f4508d
Thunraz <julian.dinges@gmail.com>
2014-12-06 10:52:42 +0100

    Added snowman graphic to the enemy class

--- source/Enemy.hx
+++ source/Enemy.hx
@@ -12,67 +12,67 @@ import flixel.util.FlxVector;
  */
 class Enemy extends FlxSprite
 {
-	
-	// this is the position, where the player is currently at.
-	private var targetPosition : FlxPoint;
-	
-	private var healthCurrent:Float;
-	private var healtMax:Float;
-	
-	private var shootTimerCurrent :Float;
-	private var shootTimerMax :Float;
-	
-	private var state : PlayState;
-	
-	public function new(X:Float=0, Y:Float=0, playstate:PlayState) 
-	{
-		super(X, Y);
-		
-		state = playstate;
-		
-		makeGraphic(24, 24, FlxColorUtil.makeFromARGB(1.0, 255, 125, 125));
-		targetPosition = new FlxPoint();
-		
-	}
-	
-	override public function update():Void 
-	{
-		super.update();
-		getInput();
-		doMovement();
-		
-		targetPosition = state.getPlayerPosition();
-			
-		if (shootTimerCurrent >= 0)
-		{
-			shootTimerCurrent -= FlxG.elapsed;
-		}
-	}
-	
-	public override function draw() :Void
-	{
-		super.draw();
-	}
-	
-	private function getInput() :Void 
-	{
-		
-	}
-	private function doMovement() :Void
-	{
-		velocity.x *= GameProperties.EnemyMovementVelocityDrag;
+    
+    // this is the position, where the player is currently at.
+    private var targetPosition : FlxPoint;
+
+    private var healthCurrent:Float;
+    private var healtMax:Float;
+
+    private var shootTimerCurrent :Float;
+    private var shootTimerMax :Float;
+
+    private var state : PlayState;
+
+    public function new(X:Float=0, Y:Float=0, playstate:PlayState) 
+    {
+        super(X, Y);
+        
+        state = playstate;
+        
+        loadGraphic(AssetPaths.snowman__png, false);
+        targetPosition = new FlxPoint();
+        
+    }
+    
+    override public function update():Void 
+    {
+        super.update();
+        getInput();
+        doMovement();
+        
+        targetPosition = state.getPlayerPosition();
+            
+        if (shootTimerCurrent >= 0)
+        {
+            shootTimerCurrent -= FlxG.elapsed;
+        }
+    }
+    
+    public override function draw() :Void
+    {
+        super.draw();
+    }
+    
+    private function getInput() :Void 
+    {
+        
+    }
+    private function doMovement() :Void
+    {
+        velocity.x *= GameProperties.EnemyMovementVelocityDrag;
         velocity.y *= GameProperties.EnemyMovementVelocityDrag;
-		
-		// turning stuff
-		
-		var dir : FlxVector = new FlxVector( targetPosition.x - x, targetPosition.y - y );
-		angle = dir.degrees;
-		
-		dir.normalize();
-		
-		velocity.x += dir.x * GameProperties.EnemyMovementVelocityAdd;
-		velocity.y += dir.y * GameProperties.EnemyMovementVelocityAdd;
-		
-	}
-	
+        
+        // turning stuff
+        
+        var dir : FlxVector = new FlxVector( targetPosition.x - x, targetPosition.y - y );
+        angle = dir.degrees;
+        
+        dir.normalize();
+        
+        velocity.x += dir.x * GameProperties.EnemyMovementVelocityAdd;
+        velocity.y += dir.y * GameProperties.EnemyMovementVelocityAdd;
+        
+    }
+    
 }
